cd57fe41c459d93045925bff7b585ea447d697ac
Jenny Han <12226528+bebetterjenny@users.noreply.github.com>
2023-09-22 11:34:14 +0800

    show case list

---
+++ src/pages/dashboard/Cases/constants.js
@@ -0,0 +1,38 @@
+export const mockCases = [
+    {
+        taskId: "1",
+        tscAdministratorId: "2",
+        navigatorId: "4",
+        taskProgressEnum: "START" //START, IN_PROGRESS, COMPLETED
+    },
+    {
+        taskId: "2",
+        tscAdministratorId: "4",
+        navigatorId: "3",
+        taskProgressEnum: "START"
+    },
+    {
+        taskId: "3",
+        tscAdministratorId: "2",
+        navigatorId: "5",
+        taskProgressEnum: "IN_PROGRESS"
+    },
+    {
+        taskId: "4",
+        tscAdministratorId: "3",
+        navigatorId: "2",
+        taskProgressEnum: "COMPLETED"
+    },
+    {
+        taskId: "5",
+        tscAdministratorId: "5",
+        navigatorId: "1",
+        taskProgressEnum: "COMPLETED"
+    },
+    {
+        taskId: "6",
+        tscAdministratorId: "1",
+        navigatorId: "1",
+        taskProgressEnum: "IN_PROGRESS"
+    }
+]
--- src/pages/dashboard/Cases/index.js
+++ src/pages/dashboard/Cases/index.js
@@ -7,6 +7,7 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import DownloadIcon from '@mui/icons-material/Download';
 import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
 import { useNavigate } from 'react-router-dom';
 import {
     GridRowsProp,
@@ -27,6 +28,7 @@ import {
     randomId,
     randomArrayItem,
   } from '@mui/x-data-grid-generator';
+import { mockCases } from './constants';
 
 // styles
 const StyledRoot = styled(MainCard)(({ theme }) => ({
@@ -36,17 +38,18 @@ const StyledRoot = styled(MainCard)(({ theme }) => ({
 
 }));
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const rows = mockCases;
+// [
+//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
 
 const Cases = () => {
     const navigate = useNavigate();
@@ -56,38 +59,40 @@ const Cases = () => {
     }, [navigate]);
 
     const columns = useMemo(() => [
-        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'taskId', headerName: 'Case ID', flex: 1 },
         {
-            field: 'firstName',
-            headerName: 'First name',
-            width: 150,
-            // editable: true,
+            field: 'tscAdministratorId',
+            headerName: 'TSC Admin ID', 
+            flex: 1,
         },
         {
-            field: 'lastName',
-            headerName: 'Last name',
-            width: 150,
+            field: 'navigatorId',
+            headerName: 'Navigator ID',
+            flex: 1,
         },
         {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 110,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
+            field: 'taskProgressEnum',
+            headerName: 'Progress',
             sortable: false,
-            width: 160,
-            valueGetter: (params) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+            flex: 1,
+            renderCell: (params) => {
+                switch (params.value) {
+                    case 'START':
+                        return <Chip label={params.value} color="info" variant="outlined" />;
+                    case 'IN_PROGRESS':
+                        return <Chip label={params.value} color="primary" variant="outlined" />;
+                    case 'COMPLETED':
+                        return <Chip label={params.value} color="success" variant="outlined" />;
+                    default:
+                        return <Chip label={params.value} color="default" variant="outlined" />;
+                }
+            },
         },
         {
             field: 'actions',
             type: 'actions',
             headerName: 'Actions',
-            width: 100,
+            flex: 1,
             cellClassName: 'actions',
             getActions: ({ id }) => {
 
@@ -115,6 +120,7 @@ const Cases = () => {
             <DataGrid
                 rows={rows}
                 columns={columns}
+                getRowId={(row) => row.taskId}
                 initialState={{
                     pagination: {
                         paginationModel: {
